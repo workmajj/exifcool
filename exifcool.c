e5bbb52ddc81d7d4f4d257e6132a667e8d783684
John J. Workman <johnjworkman@gmail.com>
2015-08-18 18:42:29 -0400

    Parse args and print dir listing

--- exifcool.c
+++ exifcool.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
-    printf("hi hi\n");
+    if (2 != argc) {
+        printf("usage: %s <dir>\n", argv[0]);
+        return 1;
+    }
 
+    DIR *dp;
+
+    dp = opendir(argv[1]);
+    if (NULL == dp) {
+        printf("couldn't open directory %s\n", argv[1]); // TODO: perror?
+        return 1;
+    }
+
+    struct dirent *ep;
+    while ((ep = readdir(dp))) {
+        printf("%s\n", ep->d_name);
+    }
+
+    closedir(dp);
     return 0;
 }
