6765ff2d0ebcab88c867f1635ffeeafe6a491539
John J. Workman <johnjworkman@gmail.com>
2015-08-21 15:19:21 -0400

    Filter dir listing on file exts

--- exifcool.c
+++ exifcool.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+
+int _ec_file_filter(const struct dirent *ep, const char *ext)
+{
+    if (!ep || !ext) return 0;
+
+    if (ep->d_type != DT_REG) return 0; // not a regular file
+
+    int lenext = strlen(ext); // FIXME: strnlen?
+    if (lenext > ep->d_namlen) return 0;
+
+    return (strncmp(ep->d_name + ep->d_namlen - lenext, ext, lenext) == 0);
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("usage: %s <dir>\n", argv[0]);
+    if (argc != 3) {
+        printf("usage: %s <dir> <ext>\n", argv[0]);
         return 1;
     }
 
@@ -17,7 +30,7 @@ int main(int argc, char *argv[])
 
     struct dirent *ep;
     while ((ep = readdir(dp))) {
-        if (ep->d_type != DT_REG) continue; // TODO: abstract and check ext
+        if (!_ec_file_filter(ep, argv[2])) continue;
 
         printf("%s\n", ep->d_name);
     }
