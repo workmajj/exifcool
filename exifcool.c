7e0af82c81baad6f6b504758210bf0b194598798
John J. Workman <johnjworkman@gmail.com>
2015-08-25 11:49:26 -0400

    First pass at printing datetime EXIF tag

--- exifcool.c
+++ exifcool.c
@@ -2,6 +2,8 @@
 #include <dirent.h>
 #include <string.h>
 
+#include <libexif/exif-data.h>
+
 int _ec_file_filter(const struct dirent *ep, const char *ext)
 {
     if (!ep || !ext) return 0;
@@ -14,6 +16,40 @@ int _ec_file_filter(const struct dirent *ep, const char *ext)
     return (strncmp(ep->d_name + ep->d_namlen - lenext, ext, lenext) == 0);
 }
 
+void _ec_exif_print_date(const ExifData *ed)
+{
+    if (!ed) return;
+
+    ExifEntry *entry = exif_content_get_entry(ed->ifd[EXIF_IFD_0],
+        EXIF_TAG_DATE_TIME);
+    if (!entry) return;
+
+    char buf[1024];
+    exif_entry_get_value(entry, buf, sizeof(buf));
+
+    if (*buf) {
+        printf("%s: %s\n", exif_tag_get_name_in_ifd(EXIF_TAG_DATE_TIME,
+            EXIF_IFD_0), buf);
+    }
+}
+
+void _ec_exif_print(const struct dirent *ep)
+{
+    if (!ep) return;
+
+    ExifData *ed;
+
+    ed = exif_data_new_from_file(ep->d_name);
+    if (!ed) {
+        printf("couldn't get exif data from file %s\n", ep->d_name); // TODO: perror?
+        return;
+    }
+
+    _ec_exif_print_date(ed);
+
+    exif_data_unref(ed);
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
@@ -23,7 +59,8 @@ int main(int argc, char *argv[])
 
     DIR *dp;
 
-    if (!(dp = opendir(argv[1]))) {
+    dp = opendir(argv[1]);
+    if (!dp) {
         printf("couldn't open directory %s\n", argv[1]); // TODO: perror?
         return 1;
     }
@@ -32,7 +69,7 @@ int main(int argc, char *argv[])
     while ((ep = readdir(dp))) {
         if (!_ec_file_filter(ep, argv[2])) continue;
 
-        printf("%s\n", ep->d_name);
+        _ec_exif_print(ep);
     }
 
     closedir(dp);
