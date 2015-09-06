e9a41d419ed143f524fd02ec71f3bb650ea854c1
John J. Workman <johnjworkman@gmail.com>
2015-09-06 19:27:10 -0400

    First pass at DigitTrie for indexed counts

--- exifcool.c
+++ exifcool.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <libexif/exif-data.h>
@@ -11,6 +12,111 @@
 #define EC_EXIF_TAG EXIF_TAG_DATE_TIME
 
 #define EC_EXIF_TAG_BYTES 20 // per exif spec for datetime tags
+
+/* DigitTrie */
+
+// TODO: move to external header+source files
+// TODO: set func storage classes
+
+// all keys are uniform-length strings of digits (e.g., "20140830143607")
+
+size_t dt_length = 0; // uninitialized value
+
+typedef struct DigitTrieNode {
+    struct DigitTrieNode *children[10]; // digits 0-9
+    size_t count;
+} dt_node_t;
+
+dt_node_t *dt_alloc()
+{
+    dt_node_t *n = malloc(sizeof(dt_node_t));
+    assert(n != NULL);
+
+    for (size_t i = 0; i < 10; i++) n->children[i] = NULL;
+
+    return n;
+}
+
+dt_node_t *dt_init(const size_t length)
+{
+    assert(length > 0 && length < SIZE_MAX);
+    dt_length = length;
+
+    return dt_alloc();
+}
+
+void dt_free(dt_node_t *n)
+{
+    assert(n != NULL);
+
+    for (size_t i = 0; i < 10; i++) {
+        if (n->children[i] != NULL) dt_free(n->children[i]);
+    }
+
+    free(n);
+}
+
+void dt_destroy(dt_node_t *root)
+{
+    assert(dt_length > 0); // init-ed
+    dt_length = 0;
+
+    dt_free(root);
+}
+
+int dt_ctoi(const char *c)
+{
+    char a[2] = {c[0], '\0'};
+
+    return atoi(a);
+}
+
+void dt_set(dt_node_t *root, const char *digit_str, const size_t count)
+{
+    assert(dt_length > 0); // init-ed
+
+    assert(root != NULL);
+    assert(digit_str != NULL);
+    assert(count < SIZE_MAX);
+
+    size_t len = strnlen(digit_str, dt_length + 1);
+    assert(len == dt_length);
+
+    dt_node_t *curr = root;
+
+    for (size_t i = 0; i < len; i++) {
+        int idx = dt_ctoi(&digit_str[i]);
+
+        if (!curr->children[idx]) curr->children[idx] = dt_alloc();
+
+        curr = curr->children[idx];
+    }
+
+    curr->count = count;
+}
+
+size_t dt_get(dt_node_t *root, const char *digit_str)
+{
+    assert(dt_length > 0); // init-ed
+
+    assert(root != NULL);
+    assert(digit_str != NULL);
+
+    size_t len = strnlen(digit_str, dt_length + 1);
+    assert(len == dt_length);
+
+    dt_node_t *curr = root;
+
+    for (size_t i = 0; i < len; i++) {
+        int idx = dt_ctoi(&digit_str[i]);
+
+        if (!curr->children[idx]) return 0; // not yet set
+
+        curr = curr->children[idx];
+    }
+
+    return curr->count;
+}
 
 /* file */
 
